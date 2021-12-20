5ba6fb8ab4c1bc3e554a40290d2ac89c3e848aae
kcamp23 <89957266+kcamp23@users.noreply.github.com>
2021-12-20 17:08:31 -0500

    my first change

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,8 +1,10 @@
 package com.careerdevs;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        System.out.println("hello github " + Arrays.deepToString(args));
     }
-}
