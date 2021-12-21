1fe5f72a300ed2f2882e92949678489f7329154b
kcamp23 <89957266+kcamp23@users.noreply.github.com>
2021-12-20 19:57:46 -0500

    java 6-9 added

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,10 +1,70 @@
 package com.careerdevs;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("hello github ");
+    }
+}
+
+//alg 1
+public static int getFirstValue(int[] arr) {
+    return arr[0];
+}
+
+//alg 2
+public static int addition(int num) {
+    return ++num;
+}
+
+//alg 3
+public static int search(int arr[], int item) {
+    for(int i =0; i<arr.length; i++)
+        if(arr[i]==item)
+            return i;
+    return -1;
+}
+
+//alg 4
+public static int convert(int minutes) {
+    return minutes * 60;
+}
+
+//alg 5
+public static String giveMeSomething(String a) {
+    return ("something " + a);
+}
+
+
+// alg 6
+    public static boolean reverse(boolean b) {
+        if (b)
+            return false;
+        else
+            return true;
+    }
+
+
+// alg 7
+
+    public static int countWords(String s) {
+        return s.split(" ").length;
+    }
+
+
+
+    //alg 8
+
+    public static boolean existsHigher(int[] arr, int n) {
+        if (arr.length == 0) return false;
+        for (int i : arr) {
+            if (i >= n) return true;
+        }
+        return false;
+    }
+
+    //alg 9
+
+    public static int getCount(String str) {
+        return str.replaceAll("[^aeiouAEIOU]", "").length();
     }
